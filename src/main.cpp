e56d72ada4203abba784b1270ef1a19bc94baac4
Nina <259290@pwr.edu.pl>
2021-03-19 23:31:43 +0100

    Dodane proste wywołania w petli w celu sprawdzenia poprawnosci wczytywania i wyswietlania liczb

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 #include "BazaTestu.hh"
+#include "LZespolona.hh"
+#include "WyrazenieZesp.hh"
 
 using namespace std;
 
-
-
-
 int main(int argc, char **argv)
 {
+  LZespolona zesp1, zesp2;
+  zesp1.re = 1;
+  zesp1.im = 2;
 
-  if (argc < 2) {
+  if (argc < 2)
+  {
     cout << endl;
     cout << " Brak opcji okreslajacej rodzaj testu." << endl;
     cout << " Dopuszczalne nazwy to:  latwy, trudny." << endl;
@@ -17,30 +20,30 @@ int main(int argc, char **argv)
     return 1;
   }
 
+  BazaTestu BazaT = {nullptr, 0, 0};
 
-  BazaTestu   BazaT = { nullptr, 0, 0 };
-
-  if (InicjalizujTest(&BazaT,argv[1]) == false) {
+  if (InicjalizujTest(&BazaT, argv[1]) == false)
+  {
     cerr << " Inicjalizacja testu nie powiodla sie." << endl;
     return 1;
   }
 
-
-  
   cout << endl;
   cout << " Start testu arytmetyki zespolonej: " << argv[1] << endl;
   cout << endl;
 
-  WyrazenieZesp   WyrZ_PytanieTestowe;
-  
-  while (PobierzNastpnePytanie(&BazaT,&WyrZ_PytanieTestowe)) {
-    cout << " Czesc rzeczywista pierwszego argumentu: ";
-    cout << WyrZ_PytanieTestowe.Arg1.re << endl;
+  WyrazenieZesp WyrZ_PytanieTestowe;
+  LZespolona zespolona;
+
+  while (PobierzNastpnePytanie(&BazaT, &WyrZ_PytanieTestowe))
+  {
+    cout << " Podaj wynik wyrazenia:  ";
+    Wyswietl(WyrZ_PytanieTestowe); /*Sprawdzenie wyświetlania pytania*/
+    Wczytaj(zespolona);/*Sprawdzenie wczytywania liczby zespolonej*/
+    cout<<endl;
   }
 
-  
   cout << endl;
   cout << " Koniec testu" << endl;
   cout << endl;
-
 }
