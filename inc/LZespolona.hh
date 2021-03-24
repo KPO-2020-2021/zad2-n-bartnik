9897609901cdbebcf6f077a6d65b9288151a5cfc
Nina <259290@pwr.edu.pl>
2021-03-24 18:39:22 +0100

    Dodane operatory przesuniecia bitowego i porownania

--- inc/LZespolona.hh
+++ inc/LZespolona.hh
@@ -26,10 +26,11 @@ LZespolona  operator - (LZespolona  Skl1,  LZespolona  Skl2);
 LZespolona  operator * (LZespolona  Skl1,  LZespolona  Skl2);
 LZespolona  operator / (LZespolona  Skl1,  LZespolona  Skl2);
 LZespolona  operator / (LZespolona  Skl1,  double  Skl2);
-/*istream& operator >> (istream &StrWej, LZespolona Skl1); Naglowek funkcji wczytującej wyrazenie*/
-void Wczytaj (LZespolona &Skl1);
+istream& operator >> (istream &StrWej, LZespolona &Skl1); /*Naglowek funkcji wczytującej wyrazenie*/
+ostream& operator << (ostream &StrWyj, LZespolona Skl1); /*Naglowek funkcji wyczytującej wyrazenie*/
 LZespolona Sprzezenie(LZespolona Skl2);
+bool operator==(LZespolona Skl1, LZespolona Skl2);/*Naglowek funkcji porównującej liczby*/
 double Modul2(LZespolona Skl2);
-void Wyswietl(LZespolona);
+
 
 #endif
