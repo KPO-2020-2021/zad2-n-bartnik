ba581c096cfb067bfd1e6056c4777a16b4081233
Nina <259290@pwr.edu.pl>
2021-03-24 18:41:06 +0100

    Dodana struktura statystyki i naglowki funkcji

--- inc/Statystyki.hh
+++ inc/Statystyki.hh
@@ -1,13 +1,23 @@
-#ifndef WYRAZENIEZESP_HH
-#define WYRAZENIEZESP_HH
-
-
+#ifndef STATYSTYKA_HH
+#define STATYSTYKA_HH
 
 #include "LZespolona.hh"
 #include "WyrazenieZesp.hh"
 
-/*
- * Tu nalezy zdefiniowac funkcje, ktore definiuja model statystyki 
- */
+/*Struktura dla statystyki*/
+struct Statystyka
+{
+    int punkty; /*Punkty za poprawne odpowiedzi*/
+    int pytania; /*Ilosc pytan*/
+    double procent; /*Procent poprawnych odpowiedzi*/
+    double procentz; /*Procnt złych odpowiedzi*/
+};
 
+/*Nagłówki wszystkich funkcji występujących w pliku Statystyki.cpp*/
+void Inicjuj(Statystyka &stat);
+void Wyswietl(Statystyka stat);
+void Oblicz(Statystyka &stat);
+void Zlicz_pyt(Statystyka &stat);
+void Zlicz_pkt(Statystyka &stat);
+void Oblicz_zle(Statystyka &stat);
 #endif
